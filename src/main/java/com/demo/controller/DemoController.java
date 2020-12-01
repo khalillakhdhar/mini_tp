efcc201b2eea6fc04ec0bb81ffeb80046e0237fb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-01 16:03:22 +0100

    Update DemoController.java

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,5 +1,14 @@
 package com.demo.controller;
 
-public class DemoController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class DemoController {
+	@GetMapping("/api")
+	public String getFoos(@RequestParam(required = false) String id) {
+	return "ID: " + id;
+	}
 }
