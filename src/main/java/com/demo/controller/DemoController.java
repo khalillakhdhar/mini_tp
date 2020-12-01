e78d54ebee521dfcae6b8390a4bf1c362d49ebfb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-01 16:12:55 +0100

    newsletter

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,9 +1,11 @@
 package com.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class DemoController {
@@ -15,5 +17,21 @@ public class DemoController {
 	@ResponseBody
 	public String getting(@RequestParam(defaultValue = "test") String id) {
 	return "ID: " + id;
+	}
+	@GetMapping("page")
+	public ModelAndView pageweb()
+	{
+		ModelAndView mapage=new ModelAndView("newsletter.html");
+		return mapage;
+		
+		
+	}
+	@PostMapping("news")
+	@ResponseBody
+	public String news(@RequestParam String letter)
+	{
+		return letter+" inscrit avec succés";
+		
+		
 	}
 }
